e3c71a97468c79b9f58bded01933d66a8171f74c
Danilo Bettencourt <D.J.BettencourtdeFreitas@student.tudelft.nl>
2021-06-13 22:39:44 +0200

    changed alpha value

--- mmseNoiseReduce.m
+++ mmseNoiseReduce.m
@@ -9,7 +9,7 @@ function [out, eSigma] = mmseNoiseReduce(in, prev, eSigmaPrev, prior)
     E = ((1/(1+xi)^2) + (xi/((1+xi)*prior)))*amp2;
     % step 3. get xiDD using decision-directed approach https://ieeexplore.ieee.org/abstract/document/1164453
     eA = (E/(1+E))*abs(prev);
-    x = 0.8;
+    x = 0.96; % according to slides this should be between 0.96 - 0.99
     xidd = x*(eA/eSigmaPrev) + (1-x)*max(0, y - 1);
     % step 4. B(xidd)
     g = gammainc(2, 1/(1+Edd));
