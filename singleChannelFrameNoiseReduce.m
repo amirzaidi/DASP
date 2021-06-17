c33f9fd36056a77980964a866c64df5c3bfc5906
Amir Zaidi <azaidi@live.nl>
2021-06-17 22:15:43 +0200

    Remove unused files and add results

--- singleChannelFrameNoiseReduce.m
+++
@@ -1,8 +0,0 @@
-function [out, posterior] = singleChannelFrameNoiseReduce(in, history, prior)
-    amp = abs(in);
-    phase = angle(in);
-    noise = min(abs(history), [], 2);
-    amp = max(min(amp, 0.01), amp - 16 * noise);
-    out = amp .* exp(1i .* phase);
-    posterior = prior;
-end
